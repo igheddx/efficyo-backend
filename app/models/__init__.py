aa88a1ee54405bda99808e853207c950dc282c8c
Dominic Ighedosa <ighedosa.me@gmail.com>
2026-04-02 12:48:00 -0500

    Fix app.models exports to match actual ORM class names

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,43 +6,50 @@ when the ORM configures mappers in worker/API processes.
 
 from app.models.access_grant import AccessGrant
 from app.models.account_tag_key import AccountTagKey
-from app.models.approval_request import ApprovalRequest
+from app.models.approval_request import ApprovalAssignment, ApprovalRequest
 from app.models.cloud_account import CloudAccount
-from app.models.cost_snapshot import CostSnapshot
+from app.models.cost_snapshot import CostApiUsageLog, CostFetchLock, CostSnapshot, CostSyncPolicy
 from app.models.execution_audit_event import ExecutionAuditEvent
-from app.models.execution_owner import ExecutionOwner
+from app.models.execution_owner import ExecutionOwnerAssignment
 from app.models.execution_policy import ExecutionPolicy
 from app.models.finding import Finding
 from app.models.ingestion_job import IngestionJob
 from app.models.notification import Notification
-from app.models.organization import Organization
+from app.models.organization import OrgMembership, Organization
 from app.models.policy_profile import PolicyProfile
 from app.models.recommendation import Recommendation
 from app.models.recommendation_outcome import RecommendationOutcome
 from app.models.resource_snapshot import ResourceSnapshot
-from app.models.sync_pipeline import SyncTask, SyncTaskDependency
+from app.models.sync_pipeline import SyncJob, SyncJobEvent, SyncTask
 from app.models.tenant import Tenant
-from app.models.user import User
+from app.models.user import AuthSession, User
 
 __all__ = [
 	"AccessGrant",
 	"AccountTagKey",
+	"ApprovalAssignment",
 	"ApprovalRequest",
 	"CloudAccount",
+	"CostApiUsageLog",
+	"CostFetchLock",
 	"CostSnapshot",
+	"CostSyncPolicy",
 	"ExecutionAuditEvent",
-	"ExecutionOwner",
+	"ExecutionOwnerAssignment",
 	"ExecutionPolicy",
 	"Finding",
 	"IngestionJob",
 	"Notification",
+	"OrgMembership",
 	"Organization",
 	"PolicyProfile",
 	"Recommendation",
 	"RecommendationOutcome",
 	"ResourceSnapshot",
+	"SyncJob",
+	"SyncJobEvent",
 	"SyncTask",
-	"SyncTaskDependency",
 	"Tenant",
+	"AuthSession",
 	"User",
 ]
